30ce30923d1a3e1c6829d91914f1304a1c61d8b1
ChoppyChoppa <nonthananhinmalai@gmail.com>
2026-01-07 02:03:02 +0700

    Completed

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -19,3 +19,38 @@ int main(){
     cout << "Min = " << B[5];
     return 0;
 }
+
+void stat(const double A[],int N,double B[]){
+    for(int i = 0;i < N;i++){
+        B[0] += A[i];
+    }
+    B[0] /= N; /*Arithmetic Mean*/
+
+    double sum = 0, sum_sqrt = 0;
+    for(int i = 0; i < N;i++) sum = sum + A[i];
+    for(int i = 0; i < N;i++) sum_sqrt = sum_sqrt + pow(A[i],2);
+    double mean = sum/N;
+    B[1] = sqrt((sum_sqrt/N) - pow(mean,2)); /*Standard Deviation*/
+    
+    double s = 1;
+    for(int i = 0;i < N;i++) s *= A[i];
+    B[2] = pow(s,1.0/N); /*Geometric Mean*/ 
+
+    for(int i = 0;i < N;i++) B[3] += 1/A[i];
+    B[3] = N / B[3]; /*Harmonic Mean*/
+
+    for(int i = 0;i < N;i++){
+        if (A[i] > B[4]){
+            B[4] = A[i];
+        }
+    } /*Max*/
+
+    B[5] = A[0];
+    for(int i = 0;i < N;i++){
+        if (B[5] < A[i]){
+            continue;
+        }else{
+            B[5] = A[i];
+        }
+    } /*Min*/
+}
