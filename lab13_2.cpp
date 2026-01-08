dffa3a0ea4ca531d01aff8264e78bf243c14ce38
ChoppyChoppa <nonthananhinmalai@gmail.com>
2026-01-08 20:41:43 +0700

    Completed

--- lab13_2.cpp
+++ lab13_2.cpp
@@ -21,3 +21,32 @@ int main()
     }while(s != 0 || x != 0 || y != 0);
     return 0;
 }
+
+void updateImage(bool image[][M],int s,int x,int y){
+    for (int i = 0;i < N;i++){
+        for (int j = 0;j < M;j++){
+            double d = sqrt(pow(i - x, 2) + pow(j - y, 2));
+            if (d <= s-1){
+                image[i][j] = 1;
+            }
+        }
+    }
+}
+
+void showImage(const bool image[][M]){
+    for (int i = -1; i < N+1; i++){
+        for (int j = -1; j < M+1; j++){
+            if (i == -1 || i == N){
+                cout << "-";
+            }else if (j == -1 || j == M){
+                cout << "|";
+            }
+            else if (image[i][j] == 1){
+                cout << "*";
+            }else{
+                cout << " ";
+            }
+        }
+        cout << endl;
+    }
+}
